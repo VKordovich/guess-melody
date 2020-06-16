a32f7c458daa20339ee331a114cac1d5724fc5ad
VKordovich <vkordovich@gmail.com>
2020-06-17 00:05:29 +0700

    Вторая страница

--- js/screens/levelArtist.js
+++ js/screens/levelArtist.js
@@ -1,54 +1,106 @@
-import {getElementFromTemplate, showScreen} from '../utils';
+import {getElementFromTemplate, showScreen, arrayShuffle, getRandomInt, padLeft} from '../utils';
 import levelGenre from './levelGenre';
+import tracks from '../models/tracks';
 
-const html = `<section class="main main--level main--level-artist">
-    <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+const MAX_ANSWERS_SHOW = 3;
+const LEVEL_TIME = 10;
+const TIMER_TIMEOUT = 1000;
+let gameState;
+let timerNode;
+let timerIntervalId;
+
+const createTimerHtml = (time = 0) => {
+  time = LEVEL_TIME - time;
+  const minutes = Math.floor(time / 60);
+  const seconds = time - minutes * 60;
+
+  return `<span class="timer-value-mins">${padLeft(minutes)}</span>
+          <!----><span class="timer-value-dots">:</span>
+          <!----><span class="timer-value-secs">${padLeft(seconds)}</span>`;
+};
+
+const startTimer = () => {
+  if (!timerIntervalId) {
+    clearInterval(timerIntervalId);
+  }
+
+  const updateTimer = () => {
+    gameState.levelArtist.time++;
+    timerNode.innerHTML = createTimerHtml(gameState.levelArtist.time);
+    if (gameState.levelArtist.time === LEVEL_TIME) {
+      clearInterval(timerIntervalId);
+      showScreen(levelGenre(gameState));
+    }
+  };
+
+  timerIntervalId = setInterval(updateTimer, TIMER_TIMEOUT);
+};
+
+const createLevel = () => {
+
+  const options = arrayShuffle(Array.from(tracks)).slice(0, MAX_ANSWERS_SHOW);
+  const levelHistory = {answerId: null, optionId: null, optionsId: options.map(([index]) => index)};
+  const answers = [];
+
+  levelHistory.answerId = levelHistory.optionsId[getRandomInt(0, levelHistory.optionsId.length)];
+  gameState.levelArtist.levelHistory.set(gameState.levelArtist.level, levelHistory);
+
+  options.map(([index, track]) => {
+    answers.push(`<div class="main-answer-wrapper">
+                    <input class="main-answer-r" type="radio" id="answer-${index}" name="answer" value="${index}" />
+                    <label class="main-answer" for="answer-${index}">
+                      <img class="main-answer-preview" src="${track.imgSrc}">
+                      ${track.title}
+                    </label>
+                  </div>`);
+  });
+
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
       <circle
         cx="390" cy="390" r="370"
         class="timer-line"
         style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
 
-      <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-        <span class="timer-value-mins">02</span><!--
-        --><span class="timer-value-dots">:</span><!--
-        --><span class="timer-value-secs">00</span>
-      </div>
-    </svg>
+        <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+          ${createTimerHtml(gameState.levelArtist.time)}
+        </div>
+    </svg>`;
 
+  const title = `<h2 class="title main-title">Кто исполняет эту песню?</h2>`;
+
+  const emptyString = ``;
+  const html = `<section class="main main--level main--level-artist">
+    ${svg}
     <div class="main-wrap">
       <div class="main-timer"></div>
 
-      <h2 class="title main-title">Кто исполняет эту песню?</h2>
+      ${title}
       <div class="player-wrapper"></div>
       <form class="main-list">
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-1" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-1">
-            <img class="main-answer-preview" src="">
-            Пелагея
-          </label>
-        </div>
-
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-2" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-2">
-            <img class="main-answer-preview" src="">
-            Краснознаменная дивизия имени моей бабушки
-          </label>
-        </div>
-
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-2" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-2">
-            <img class="main-answer-preview" src="">
-            Lorde
-          </label>
-        </div>
+      ${answers.join(emptyString)}
       </form>
     </div>
   </section>`;
-const element = getElementFromTemplate(html);
-const arrayButtons = Array.from(element.querySelectorAll(`.main-answer`));
+
+  const element = getElementFromTemplate(html);
+
+  timerNode = element.querySelector(`.timer-value`);
+
+  Array.from(element.querySelectorAll(`.main-answer-r`)).forEach((el, index, array) => {
+    el.addEventListener(`click`, (evt) => {
+      const optionId = +evt.target.value;
+      const history = gameState.levelArtist.levelHistory.get(gameState.levelArtist.level);
+      history.optionId = optionId;
+      if (history.answerId === optionId) {
+        gameState.score++;
+      }
+
+      showScreen(createLevel());
+    });
+  });
+
+  return element;
+};
 
 arrayButtons.forEach((currentButton, index, array) => {
   currentButton.addEventListener(`click`, () => {
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,9 @@
 const mainNode = document.querySelector(`.main`);
 
+export const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 export const getElementFromTemplate = (html) => {
   const template = document.createElement(`div`);
   template.innerHTML = html.trim();
@@ -13,5 +17,18 @@ export const clearScreen = () => {
 export const showScreen = (element) => {
   clearScreen();
   mainNode.appendChild(element);
+};
+
+export const arrayShuffle = (array) => {
+  for (let i = array.length; i; i--) {
+    let j = getRandomInt(0, i);
+    [array[i - 1], array[j]] = [array[j], array[i - 1]];
+  }
+
+  return array;
+};
+
+export const padLeft = (value = 0) => {
+  return value > 9 ? value.toString() : `0${value}`;
 };
 
